5a380e0166cfb593bc1eff8327dec8877c515ca2
Avin Mathew <amathew@assetricity.com>
2014-06-26 22:02:11 +1000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Malayalam TV
+
+Aggregates RSS feeds from certain Malayalam YouTube channels and presents them in a thumbnail grid for easy selection. Uses a minimalist, responsive design to accommodate for different screen sizes. Specifically identifies iPad devices through browser detection and provides a different YouTube link that is required by Safari.
+
+See it in action: <http://avinmathew.com/tv>
---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Malayalam TV</title>
+    <link rel="apple-touch-icon" href="touch-icon-iphone.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="touch-icon-ipad.png" />
+    <style type="text/css">
+html{font-size:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}a:focus{outline:thin dotted #333;outline:5px auto -webkit-focus-ring-color;outline-offset:-2px}a:hover,a:active{outline:0}img{max-width:100%;width:auto\9;height:auto;vertical-align:middle;border:0;-ms-interpolation-mode:bicubic}body{margin:0;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size:14px;line-height:20px;color:#333;background-color:#fff}a{color:#08c;text-decoration:none}a:hover,a:focus{color:#005580;text-decoration:underline}.container{margin-right:auto;margin-left:auto;*zoom:1}.container:before,.container:after{display:table;content:"";line-height:0}p{margin:0 0 10px}h4{margin:10px 0;font-family:inherit;font-size:17.5px;font-weight:bold;line-height:20px;color:inherit;text-rendering:optimizelegibility}ul{padding:0;margin:0 0 10px 25px}li{line-height:20px}.nav{margin-left:0;margin-bottom:20px;list-style:none}.nav>li>a{display:block}.nav>li>a:hover,.nav>li>a:focus{text-decoration:none;background-color:#eee}.navbar{overflow:visible;margin-bottom:20px;*position:relative;*z-index:2}.navbar-inner{min-height:40px;padding-left:20px;padding-right:20px;background-color:#fafafa;background-image:-moz-linear-gradient(top,#fff,#f2f2f2);background-image:-webkit-gradient(linear,0 0,0 100%,from(#fff),to(#f2f2f2));background-image:-webkit-linear-gradient(top,#fff,#f2f2f2);background-image:-o-linear-gradient(top,#fff,#f2f2f2);background-image:linear-gradient(to bottom,#fff,#f2f2f2);background-repeat:repeat-x;filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffffff',endColorstr='#fff2f2f2',GradientType=0);border:1px solid #d4d4d4;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;-webkit-box-shadow:0 1px 4px rgba(0,0,0,0.065);-moz-box-shadow:0 1px 4px rgba(0,0,0,0.065);box-shadow:0 1px 4px rgba(0,0,0,0.065);*zoom:1}.navbar-inner:after{clear:both}.navbar-inner:before,.navbar-inner:after{content:"";display:table;line-height:0}.navbar .nav{position:relative;left:0;display:block;float:left;margin:0 10px 0 0}.navbar .nav>li{float:left}.navbar .nav>li>a{float:none;padding:10px 15px 10px;color:#777;text-decoration:none;text-shadow:0 1px 0 #fff}.navbar .nav>li>a:focus,.navbar .nav>li>a:hover{background-color:transparent;color:#333;text-decoration:none}.navbar .nav>.active>a,.navbar .nav>.active>a:hover,.navbar .nav>.active>a:focus{color:#555;text-decoration:none;background-color:#e5e5e5;-webkit-box-shadow:inset 0 3px 8px rgba(0,0,0,0.125);-moz-box-shadow:inset 0 3px 8px rgba(0,0,0,0.125);box-shadow:inset 0 3px 8px rgba(0,0,0,0.125)}.thumbnails{margin-left:-20px;list-style:none;*zoom:1}.thumbnails>li{float:left;margin-bottom:20px;margin-left:20px}.thumbnail{display:block;padding:4px;line-height:20px;border:1px solid #ddd;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;-webkit-box-shadow:0 1px 3px rgba(0,0,0,0.055);-moz-box-shadow:0 1px 3px rgba(0,0,0,0.055);box-shadow:0 1px 3px rgba(0,0,0,0.055);-webkit-transition:all .2s ease-in-out;-moz-transition:all .2s ease-in-out;-o-transition:all .2s ease-in-out;transition:all .2s ease-in-out}a.thumbnail:hover,a.thumbnail:focus{border-color:#08c;-webkit-box-shadow:0 1px 4px rgba(0,105,214,0.25);-moz-box-shadow:0 1px 4px rgba(0,105,214,0.25);box-shadow:0 1px 4px rgba(0,105,214,0.25)}@media(max-width:767px){body{padding-left:20px;padding-right:20px}.container{width:auto}.thumbnails{margin-left:0}.thumbnails>li{float:none;margin-left:0}[class*="span"]{float:none;display:block;width:100%;margin-left:0;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}}@media(min-width:768px) and (max-width:979px){[class*="span"]{float:left;min-height:1px;margin-left:20px}.container{width:724px}.span4{width:228px}}@media(min-width:1200px){[class*="span"]{float:left;min-height:1px;margin-left:30px}.container{width:1170px}.span4{width:370px}.thumbnails{margin-left:-30px}.thumbnails>li{margin-left:30px}}@media(max-width:979px){body{padding-top:0}}ul.nav>li{font-size:2em}.thumbnail{overflow:hidden}@media(min-width:1200px){.thumbnail{height:350px}}@media(min-width:980px) and (max-width:1199px){.thumbnail{height:440px;width:480px}}@media(min-width:768px) and (max-width:979px){.thumbnail{height:260px}}@media(min-width:481px) and (max-width:767px){.thumbnail{overflow:visible}}@media(max-width:480px){.thumbnail{overflow:visible}}.container:after{clear:both}
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="navbar">
+        <div class="navbar-inner">
+          <ul class="nav">
+<?
+function parseRSS($url) { 
+  $feedeed = implode('', file($url));
+  $parser = xml_parser_create();
+  xml_parse_into_struct($parser, $feedeed, $valueals, $index);
+  xml_parser_free($parser);
+
+  // Construct array
+  foreach($valueals as $keyey => $valueal){
+    if($valueal['type'] != 'cdata') {
+      $item[$keyey] = $valueal;
+    }
+  }
+
+  $i = 0;
+
+  foreach($item as $key => $value){
+
+    if($value['type'] == 'open') {
+
+      $i++;
+      $itemame[$i] = $value['tag'];
+
+    } elseif($value['type'] == 'close') {
+
+      $feed = $values[$i];
+      $item = $itemame[$i];
+      $i--;
+
+      if(count($values[$i])>1){
+        $values[$i][$item][] = $feed;
+      } else {
+        $values[$i][$item] = $feed;
+      }
+
+    } else {
+      $values[$i][$value['tag']] = $value['value'];  
+    }
+  }
+
+  return $values[0];
+}
+
+function ago($time) {
+  $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+  $lengths = array("60","60","24","7","4.35","12","10");
+
+  $now = time();
+
+  $difference = $now - $time;
+  $tense = "ago";
+
+  for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+    $difference /= $lengths[$j];
+  }
+
+  $difference = round($difference);
+
+  if($difference != 1) {
+    $periods[$j].= "s";
+  }
+
+  return "$difference $periods[$j] ago";
+}
+
+$selected = $_GET["channel"];
+if (is_null($selected )) {
+  $selected = "asianetindia";
+}
+
+$channels = array('asianetglobal'=>'Asianet', 'suryatvmalayalam'=>'Surya', 'amritatv'=>'Amrita', 'kairalionline'=>'Kairali', 'asianetnews'=>'Asianet News', 'suryanewstv'=>'Surya News', 'mazhavilmanorama'=>'Manorama', 'darshanatv'=>'Darshana', 'indiavisiononlive'=>'India Vision', 'jaihindonline'=>'Jaihind', 'jeevantvchannel'=>'Jeevan', 'mcntele'=>'MCN', 'peopletvonline'=>'People', 'reporterlivechannel'=>'Reporter', 'wetvonline'=>'We TV');
+
+foreach($channels as $channel => $name) {
+  echo "<li";
+  if ($channel == $selected) {
+    echo " class='active'";
+  }
+  echo "><a href='index.php?channel={$channel}'>{$name}</a></li>";
+}
+?>
+          </ul>
+        </div>
+      </div>
+      <ul class="thumbnails">
+<?
+$xml = parseRSS("http://www.youtube.com/rss/user/{$selected}/videos.rss");
+foreach($xml['RSS']['CHANNEL']['ITEM'] as $item) {
+  $split = explode(":", $item['GUID']);
+  $id = end($split);
+  $title = $item['TITLE'];
+  $date = ago(strtotime($item['PUBDATE']));
+  echo "<li class='span4'>";
+  echo "<div class='thumbnail'>";
+  if(strpos($_SERVER['HTTP_USER_AGENT'], 'iPad') !== false) {
+    // iPad requires a different link
+    echo "<a href='http://www.youtube.com/embed/{$id}?autoplay=1'>";
+  } else {
+    echo "<a href='http://www.youtube.com/v/{$id}?autoplay=1'>";
+  }
+  echo "<img src='http://img.youtube.com/vi/{$id}/0.jpg'>";
+  echo "<h4>{$title}</h4>";
+  echo "</a>";
+  echo "<p>{$date}</p>";
+  echo "</div>";
+  echo "</li>";
+}
+?>
+      </ul>
+    </div>
+  </body>
+</html>
+<?
+// Log channel access
+//$log = $selected . "\t" . date("c") . "\n";
+//file_put_contents("selected.log", $log, FILE_APPEND | LOCK_EX);
+?>
